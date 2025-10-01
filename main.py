58348d1a5edd25387b1c0c0ded645ed4ba0bbc5a
aymbh <7@aymbh.net>
2025-10-01 17:45:46 +0300

    using hyprland to launch cvlc in fullscreen. also added an input video path for the user (this will be removed in favor of a terminal command)

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 import vlc
-import time
+import os
 
+videoPath = str(input("enter the video's path: "))
 
-player = vlc.Instance()
-
-mList = player.media_list_new()
-
-
-media = player.media_new("miku-purple.3840x2160.mp4")
-
-mList.add_media(media)
-
-
-media_player = player.media_list_player_new()
-
-media_player.set_media_list(mList)
-
-player.vlm_set_loop("miku-purple.3840x2160", True)
-
-media_player.play()
-
-input()
+os.system("""hyprctl dispatch exec [fullscreen] 'cvlc --input-repeat=999999 """ + videoPath + "'")
